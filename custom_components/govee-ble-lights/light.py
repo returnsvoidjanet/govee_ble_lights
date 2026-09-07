8d5af10b0943aa6536f71bc221234885f1ca787d
returnsvoidjanet <returnsvoidjanet@gmail.com>
2026-09-07 18:01:41 -0500

    H6053: remove temporary byte-logging

--- custom_components/govee-ble-lights/light.py
+++ custom_components/govee-ble-lights/light.py
@@ -389,7 +389,6 @@ class GoveeBluetoothLight(LightEntity):
 
         client = await self._connectBluetooth()
         for command in commands:
-            _LOGGER.warning("BYTES seg=%s %s", self._segment, command.hex())
             await client.write_gatt_char(UUID_CONTROL_CHARACTERISTIC, command, False)
             await asyncio.sleep(0.2)
 
